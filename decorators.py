c387ce179cb7ffa6209e681d6e487943b15502a8
SABBIR HOSSAIN <sabbirhossain@SABBIRs-Laptop.local>
2024-05-06 14:00:12 +0900

    decorator

--- decorators.py
+++ decorators.py
@@ -1,15 +1,33 @@
-#decorator pattern
-def my_decorator(func):
-    def warp_function(*args, **kwargs):
-        print("***********")
-        func(*args, **kwargs)
-        print("***********")
-    return warp_function
+# #decorator pattern
+# def my_decorator(func):
+#     def warp_function(*args, **kwargs):
+#         print("***********")
+#         func(*args, **kwargs)
+#         print("***********")
+#     return warp_function
 
 
-@my_decorator
-def hello(gretting, emoji=':()'):
-    print(gretting, emoji)
+# @my_decorator
+# def hello(gretting, emoji=':()'):
+#     print(gretting, emoji)
 
 
-hello("Assalamu Alaikum")
+# hello("Assalamu Alaikum")
+
+from time import time
+def performance(fn):
+    def warper(*args, **kawrgs):
+        t1 = time()
+        result = fn(*args, **kawrgs)
+        t2 = time()
+        print(f'took {t2 - t1} s')
+        return result
+    return warper
+
+@performance
+def long_time():
+    for i in range(1, 1000000):
+        i*5
+
+
+print(long_time())
