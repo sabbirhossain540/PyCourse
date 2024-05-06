04630559ab1ee85c604465eb2e7223da1bebb2db
SABBIR HOSSAIN <sabbirhossain@SABBIRs-Laptop.local>
2024-05-06 13:03:25 +0900

    dsds

--- duplicateDetector.py
+++ duplicateDetector.py
@@ -4,3 +4,6 @@ userInput = input("Enter your text: ")
 my_list = [char for char in userInput]
 duplicate = set([x for x in my_list if my_list.count(x) > 1])
 print(duplicate)
+
+
+#first code commit
