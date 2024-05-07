c2783f23ce0c8540e7f781e04be0f0d9044d9e16
SABBIR HOSSAIN <sabbirhossain@SABBIRs-Laptop.local>
2024-05-07 12:01:22 +0900

    d

--- Error_handaling.py
+++ Error_handaling.py
@@ -1,17 +1,21 @@
 
-# while True:
-#     try:
-#         age = int(input("Enter your age: "))
-#         10/age
-#     except ValueError:
-#         print("Please enter a number")
-#     else:
-#         print("Thank you")
-#         break
-
-def sum(num1, num2):
+while True:
     try:
-        return num1 + num2
-    except:
-        print("Something is wrong. Please enter Number")
-print(sum(2, 1))
+        age = int(input("Enter your age: "))
+        10/age
+    except ValueError:
+        print("Please enter a number")
+    except ZeroDivisionError:
+        print("please rnter number with zero division")
+    else:
+        print("Thank you")
+        break
+    finally:
+        print("ok, I am finally done")
+
+# def sum(num1, num2):
+#     try:
+#         return num1 + num2
+#     except:
+#         print("Something is wrong. Please enter Number")
+# print(sum(2, 1))
